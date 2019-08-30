238a701c48b9cb39778e0f49667a5ad28dba3804
Dan Traviglia <dtraviglia@gmail.com>
2019-08-30 09:21:51 -0400

    add suport for optional bucket argument

--- udn_cli/main.py
+++ udn_cli/main.py
@@ -29,24 +29,28 @@ def handle_single_upload(args):
 
 def main():
     # setup command line args
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='CLI tool for UDN sequencing file uploads')
     sub_parsers = parser.add_subparsers()
 
     # setup 'upload' command
-    single_upload_parser = sub_parsers.add_parser('upload')
-    single_upload_parser.add_argument('file_path')
-    single_upload_parser.add_argument('seq_request_id')
-    single_upload_parser.add_argument('patient_uuid')
-    single_upload_parser.add_argument('--metadata')
-    single_upload_parser.add_argument('--test', action="store_true")
-    single_upload_parser.add_argument('--force', action="store_true")
+    single_upload_parser = sub_parsers.add_parser('upload', help='upload help')
+    single_upload_parser.add_argument('file_path', help='Local path to file')
+    single_upload_parser.add_argument('seq_request_id', help='Sequence Request ID for file association')
+    single_upload_parser.add_argument('patient_uuid', help='Patient UUID for file association')
+    single_upload_parser.add_argument('--bucket', help='S3 bucket for data storage')
+    single_upload_parser.add_argument('--metadata',
+                                      help='Any extra metadata to store with the file; Needs to be JSON format')
+    single_upload_parser.add_argument('--test', action="store_true", help='Use development server')
+    single_upload_parser.add_argument('--force', action="store_true",
+                                      help='Force upload and overwrite existing file with same filename')
     single_upload_parser.set_defaults(handler=handle_single_upload)
 
     # setup 'upload-dir' command
-    multi_upload_parser = sub_parsers.add_parser('multi-upload')
-    multi_upload_parser.add_argument('directory')
-    multi_upload_parser.add_argument('--test', action="store_true")
-    multi_upload_parser.add_argument('--force', action="store_true")
+    multi_upload_parser = sub_parsers.add_parser('multi-upload', help='multi-upload help')
+    multi_upload_parser.add_argument('directory', help='Local directory of files to upload')
+    multi_upload_parser.add_argument('--test', action="store_true", help='Use development server')
+    multi_upload_parser.add_argument('--force', action="store_true",
+                                     help='Force upload and overwrite existing files with same filenames')
     multi_upload_parser.set_defaults(handler=handle_multi_upload)
 
     # build config and run
